d79758b31d7880d722ae5df5723813e0f0a216d7
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-10-03 11:52:04 +0700

    feat: add CV

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <NotionRenderer :blockMap="blockMap" fullPage />
+</template>
+
+<script>
+export default {
+  data: () => ({ blockMap: null }),
+  async asyncData({ $notion }) {
+    // use Notion module to get Notion blocks from the API via a Notion pageId
+    const blockMap = await $notion.getPageBlocks(
+      "e643118bf60d4125a323f67092c05573"
+    );
+
+    return { blockMap };
+  },
+};
+</script>
+
+<style>
+@import "vue-notion/src/styles.css"; /* optional Notion-like styles */
+</style>
