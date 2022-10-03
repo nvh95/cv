092a72e8eafc0a9a5a947608fcc23987958cdcbf
Hung Viet Nguyen <nguyenviethunghl00@gmail.com>
2022-10-03 11:58:45 +0700

    chore: Update title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: "example",
+    title: "Hung Nguyen - Creator of Jest Preview - CV",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
